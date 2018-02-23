6fe1d4ee2864dd4d8c90a8dd0e9d757c9c010839
theRegex <keinan.jpace@gmail.com>
2018-02-23 01:06:18 -0600

    updating

---
+++ components/smallComponents/ReactionsList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class ReactionsList extends React.Component{
+	constructor(props){
+		super(props);
+		this.state = {}
+	}
+
+	render(){
+		return(
+			<div>
+				<ul></ul>
+			</div>
+			)
+	}
+}
+
+export default ReactionsList;
